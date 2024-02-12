dc501d18535fe698efd798315bfd13b86d942863
ben <ben.lee8602@gmail.com>
2024-02-12 01:15:45 -0500

    matrix generator functions

--- tests/math/transform.test.cpp
+++ tests/math/transform.test.cpp
@@ -21,3 +21,50 @@ TEST_CASE("transform vector", "[transform]") {
     expected = { 30.7302, -26.9324, 12.4667 };
     REQUIRE(equal(transform(in), expected));
 }
+
+
+TEST_CASE("translation matrix", "[transform]") {
+    Mat4x4 expected = {
+        Vec4{ 1, 0, 0, 3 },
+        Vec4{ 0, 1, 0, 6 },
+        Vec4{ 0, 0, 1, 9 },
+        Vec4{ 0, 0, 0, 1 }
+    };
+    Mat4x4 actual = translate(Vec3{ 3, 6, 9 });
+    REQUIRE(expected == actual);
+}
+
+
+TEST_CASE("2d rotation matrix", "[transform]") {
+    Mat3x3f expected = {
+        Vec3f{  0.540302f, -0.841471f,  0.0f  },
+        Vec3f{  0.841471f,  0.540302f,  0.0f  },
+        Vec3f{     0.0f,       0.0f,    1.0f  }
+    };
+    Mat3x3f actual = rotate(1.0f);
+    REQUIRE(equal(expected, actual));
+}
+
+
+TEST_CASE("3d rotation matrix", "[transform]") {
+    Mat4x4f expected = {
+        Vec4f{  0.095105f,  0.646688f, -0.756802f,  0.0f  },
+        Vec4f{ -0.311591f,  0.741385f,  0.594356f,  0.0f  },
+        Vec4f{  0.945445f,  0.179287f,  0.272012f,  0.0f  },
+        Vec4f{     0.0f,       0.0f,       0.0f,    1.0f  }
+    };
+    Mat4x4f actual = rotate(Vec3f{ 2.0f, 4.0f, 8.0f });
+    REQUIRE(equal(expected, actual));
+}
+
+
+TEST_CASE("scaling matrix", "[transform]") {
+    Mat4x4f expected = {
+        Vec4f{ 0.5f, 0.0f, 0.0f, 0.0f },
+        Vec4f{ 0.0f, 1.0f, 0.0f, 0.0f },
+        Vec4f{ 0.0f, 0.0f, 2.0f, 0.0f },
+        Vec4f{ 0.0f, 0.0f, 0.0f, 1.0f }
+    };
+    Mat4x4f actual = scale(Vec3f{ 0.5f, 1.0f, 2.0f });
+    REQUIRE(equal(expected, actual));
+}
