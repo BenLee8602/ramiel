b8dbe6c72bae46ce3d83a68cc3d96943ab51da27
ben <ben.lee8602@gmail.com>
2024-05-12 13:45:23 -0400

    add image loader

--- src/graphics/texture.h
+++ src/graphics/texture.h
@@ -1,18 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <string>
 #include <ramiel/math.h>
 
 namespace ramiel {
 
     class Texture {
-        Vec2 size;
-        size_t maxIdx;
+        Vec2u res;
+        Vec2u resClamp;
         std::vector<Vec3f> data;
     public:
-        Texture(std::string filename, bool isNormalMap = false);
+        Texture(std::vector<Vec3f>& data, Vec2u res);
+        Texture(const std::string& filename, Mat4x4f format = id<float, 4>());
         Vec3f get(const Vec2f& coords) const;
-        Vec2 getSize() const;
+        Vec2u getSize() const;
     };
 
 }
