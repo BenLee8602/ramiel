270c4439807ec96056c2084dab96d3eb48890d6b
ben <ben.lee8602@gmail.com>
2024-05-29 22:10:27 -0400

    void buffer copyable and movable

--- src/data/voidbuffer.cpp
+++ src/data/voidbuffer.cpp
@@ -8,18 +8,97 @@ namespace ramiel {
         size_t size,
         Allocator allocate,
         Deallocator deallocate,
+        Copier copy,
         size_t length
     ) : type(type)
       , size(size)
-      , length(length)
       , allocate(allocate)
       , deallocate(deallocate)
+      , copy(copy)
+      , length(length)
     {
         assert(size       && "VoidBuffer::VoidBuffer - size must not be 0");
         assert(allocate   && "VoidBuffer::VoidBuffer - must provide an allocator");
         assert(deallocate && "VoidBuffer::VoidBuffer - must provide a deallocator");
         data = length ? allocate(length) : nullptr;
     }
+
+
+    VoidBuffer::VoidBuffer(const VoidBuffer& src) : type(src.type) {
+        assert(src.copy && "VoidBuffer::VoidBuffer(VoidBuffer) - source must be copyable");
+
+        size = src.size;
+        allocate = src.allocate;
+        deallocate = src.deallocate;
+        copy = src.copy;
+        length = src.length;
+
+        data = length ? allocate(length) : nullptr;
+        char* d = static_cast<char*>(data);
+        const char* s = static_cast<const char*>(src.data);
+        for (size_t i = 0; i < length; ++i) {
+            copy(d, s);
+            d += size;
+            s += size;
+        }
+    }
+
+
+    VoidBuffer::VoidBuffer(VoidBuffer&& src) : type(src.type) {
+        size = src.size;
+        allocate = src.allocate;
+        deallocate = src.deallocate;
+        copy = src.copy;
+        data = src.data;
+        length = src.length;
+
+        src.data = nullptr;
+        src.length = 0;
+    }
+    
+
+    VoidBuffer& VoidBuffer::operator=(const VoidBuffer& src) {
+        assert(src.copy && "VoidBuffer::operator=(VoidBuffer) - source must be copyable");
+
+        if (data) deallocate(data);
+
+        type = src.type;
+        size = src.size;
+        allocate = src.allocate;
+        deallocate = src.deallocate;
+        copy = src.copy;
+        length = src.length;
+
+        data = length ? allocate(length) : nullptr;
+        char* d = static_cast<char*>(data);
+        const char* s = static_cast<const char*>(src.data);
+        for (size_t i = 0; i < length; ++i) {
+            copy(d, s);
+            d += size;
+            s += size;
+        }
+
+        return *this;
+    }
+    
+
+    VoidBuffer& VoidBuffer::operator=(VoidBuffer&& src) {
+        if (data) deallocate(data);
+
+        type = src.type;
+        size = src.size;
+        allocate = src.allocate;
+        deallocate = src.deallocate;
+        copy = src.copy;
+        data = src.data;
+        length = src.length;
+
+        src.data = nullptr;
+        src.length = 0;
+
+        return *this;
+    }
+
 
     VoidBuffer::~VoidBuffer() {
         if (data) deallocate(data);
