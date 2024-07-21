b5b110b9ed6905f36fa27705b081b7cb61c7f1ac
ben <ben.lee8602@gmail.com>
2024-07-20 23:56:09 -0400

    indent using spaces

--- src/graphics/effects.h
+++ src/graphics/effects.h
@@ -4,72 +4,72 @@
 
 namespace ramiel {
 
-	typedef CameraModifier Effect;
+    typedef CameraModifier Effect;
 
 
-	class Brightness : public Effect {
-		float brightness;
-	public:
-		Brightness(float brightness) : brightness(brightness) {}
-		virtual void run(Camera& camera) const override;
-	};
+    class Brightness : public Effect {
+        float brightness;
+    public:
+        Brightness(float brightness) : brightness(brightness) {}
+        virtual void run(Camera& camera) const override;
+    };
 
 
-	class ColorFilter : public Effect {
-		Vec3f color;
-	public:
-		ColorFilter(Vec3f color) : color(color / 255.0f) {}
-		virtual void run(Camera& camera) const override;
-	};
+    class ColorFilter : public Effect {
+        Vec3f color;
+    public:
+        ColorFilter(Vec3f color) : color(color / 255.0f) {}
+        virtual void run(Camera& camera) const override;
+    };
 
 
-	class Contrast : public Effect {
-		float contrast;
-	public:
-		Contrast(float contrast) : contrast(contrast) {}
-		virtual void run(Camera& camera) const override;
-	};
+    class Contrast : public Effect {
+        float contrast;
+    public:
+        Contrast(float contrast) : contrast(contrast) {}
+        virtual void run(Camera& camera) const override;
+    };
 
 
-	class Exposure : public Effect {
-		float exposure;
-	public:
-		Exposure(float exposure) : exposure(exposure) {}
-		virtual void run(Camera& camera) const override;
-	};
+    class Exposure : public Effect {
+        float exposure;
+    public:
+        Exposure(float exposure) : exposure(exposure) {}
+        virtual void run(Camera& camera) const override;
+    };
 
 
-	class Fog : public Effect {
-		Vec3f fogColor;
-		float fogStart;
-		float fogEnd;
-		float fogFactor;
-	public:
-		Fog(
-			Vec3f fogColor,
-			float fogStart,
-			float fogEnd
-		) :
-			fogColor(fogColor),
-			fogStart(fogStart),
-			fogEnd(fogEnd),
-			fogFactor(1.0f / (fogEnd - fogStart))
-		{}
-		virtual void run(Camera& camera) const override;
-	};
+    class Fog : public Effect {
+        Vec3f fogColor;
+        float fogStart;
+        float fogEnd;
+        float fogFactor;
+    public:
+        Fog(
+            Vec3f fogColor,
+            float fogStart,
+            float fogEnd
+        ) :
+            fogColor(fogColor),
+            fogStart(fogStart),
+            fogEnd(fogEnd),
+            fogFactor(1.0f / (fogEnd - fogStart))
+        {}
+        virtual void run(Camera& camera) const override;
+    };
 
 
-	class ToneMapping : public Effect {
-	public:
-		virtual void run(Camera& camera) const override;
-	};
+    class ToneMapping : public Effect {
+    public:
+        virtual void run(Camera& camera) const override;
+    };
 
 
-	class Saturation : public Effect {
-		float saturation;
-	public:
-		Saturation(float saturation) : saturation(saturation) {}
-		virtual void run(Camera& camera) const override;
-	};
+    class Saturation : public Effect {
+        float saturation;
+    public:
+        Saturation(float saturation) : saturation(saturation) {}
+        virtual void run(Camera& camera) const override;
+    };
 
 }
