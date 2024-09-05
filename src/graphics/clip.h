d4fd67dbd38311043c3c1f94be08fbbefc243257
ben <ben.lee8602@gmail.com>
2024-09-05 19:51:19 -0400

    move clipping code to its own file

---
+++ src/graphics/clip.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <array>
+#include <list>
+
+#include <ramiel/math.h>
+
+namespace ramiel {
+
+    using Tri = std::array<Vec4f, 3>;
+    using TriList = std::list<Tri>;
+
+    bool clip(const Vec4f& v0, const Vec4f& v1, const Vec4f& v2, TriList& clippedTris);
+
+}
