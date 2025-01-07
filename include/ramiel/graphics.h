3035f1913ca042231171f4e8b04a8bcb2233bf3e
ben <ben.lee8602@gmail.com>
2025-01-06 21:33:58 -0500

    remove old physics

--- include/ramiel/graphics.h
+++ include/ramiel/graphics.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "../src/graphics/camera.h"
-#include "../src/graphics/clip.h"
 #include "../src/graphics/effects.h"
 #include "../src/graphics/entity.h"
 #include "../src/graphics/light.h"
+#include "../src/graphics/pixelshader.h"
+#include "../src/graphics/vertexshader.h"
