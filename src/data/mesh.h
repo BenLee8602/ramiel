c944ba355a0604bdd59668b902012349bdef853c
ben <ben.lee8602@gmail.com>
2025-01-07 22:17:20 -0500

    mesh metadata and iterators
    need this so we can iterate over mesh vertices and update positions for soft bodies

--- src/data/mesh.h
+++ src/data/mesh.h
@@ -8,30 +8,52 @@ namespace ramiel {
     class Mesh {
     public:
         Mesh(
-            const std::vector<uint8_t>& attr,
             const std::vector<float>& vertices,
-            const std::vector<uint32_t>& triangles
+            const std::vector<uint32_t>& triangles,
+            const std::vector<uint8_t>& attrType
         );
         Mesh(
-            const std::vector<uint8_t>& attr,
             std::vector<float>&& vertices,
-            std::vector<uint32_t>&& triangles
+            std::vector<uint32_t>&& triangles,
+            const std::vector<uint8_t>& attrType
         );
         Mesh(const std::string& filename);
 
-        const std::vector<float>& getVertices() const;
-        const std::vector<uint32_t>& getTriangles() const;
+        Mesh(const Mesh&) = default;
+        Mesh(Mesh&&) = default;
+
+        Mesh& operator=(const Mesh&) = default;
+        Mesh& operator=(Mesh&&) = default;
+
+        float* vtxBegin();
+        float* vtxEnd();
+        uint32_t* triBegin();
+        uint32_t* triEnd();
+        
+        const float* vtxBegin() const;
+        const float* vtxEnd() const;
+        const uint32_t* triBegin() const;
+        const uint32_t* triEnd() const;
+
+        const std::vector<uint8_t>& getAttrOutType() const;
+        const std::vector<uint8_t>& getAttrOutPos() const;
+        size_t getVertexSize() const;
+
+        size_t getVertexCount() const;
+        size_t getTriangleCount() const;
 
     private:
-        friend class Entity;
-        std::vector<uint8_t> getAttrOutType() const;
-        std::vector<uint8_t> getAttrOutPos() const;
-        size_t getAttrOutSize() const;
-
-    private:
-        std::vector<uint8_t> attr;
         std::vector<float> vertices;
         std::vector<uint32_t> triangles;
+
+        std::vector<uint8_t> attrOutType;
+        std::vector<uint8_t> attrOutPos;
+        size_t vertexSize;
+
+        size_t vertexCount;
+        size_t triangleCount;
+
+        void initMetadata();
     };
 
 }
