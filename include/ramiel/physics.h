7f2b546c743a4481ca000059454a6e0857ec39ee
ben <ben.lee8602@gmail.com>
2025-01-09 18:56:54 -0500

    add softbody class
    util class for creating and managing constraints and entities for soft bodies, given a mesh. does not currently use volume constraints, only edge constraints

--- include/ramiel/physics.h
+++ include/ramiel/physics.h
@@ -3,3 +3,4 @@
 #include "../src/physics/constraint.h"
 #include "../src/physics/entity.h"
 #include "../src/physics/physics.h"
+#include "../src/physics/softbody.h"
---
+++ src/physics/softbody.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <ramiel/data.h>
+
+namespace ramiel {
+
+    class Constraint;
+    class PhysicsEntity;
+
+    class SoftBody {
+    public:
+        SoftBody(const Mesh& mesh, float k);
+        ~SoftBody();
+
+        SoftBody(const SoftBody&) = delete;
+        SoftBody& operator=(const SoftBody&) = delete;
+
+        SoftBody(SoftBody&&) = default;
+        SoftBody& operator=(SoftBody&&) = default;
+
+        operator bool() const;
+        void enable();
+        void disable();
+
+        std::shared_ptr<Mesh> getMesh();
+        void updateMesh();
+
+    public:
+        std::shared_ptr<Mesh> mesh;
+        bool enabled;
+
+        std::vector<Constraint*> constraints;
+        std::vector<PhysicsEntity*> entities;
+    };
+
+}
