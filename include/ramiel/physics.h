1dbb5abaebc0ca2fe85d3cd2e1d3a5afa249dc13
ben <ben.lee8602@gmail.com>
2025-01-24 23:13:29 -0500

    collision detection infrastructure

--- include/ramiel/physics.h
+++ include/ramiel/physics.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../src/physics/broadcollision.h"
+#include "../src/physics/collision.h"
 #include "../src/physics/constraint.h"
 #include "../src/physics/entity.h"
 #include "../src/physics/inertia.h"
