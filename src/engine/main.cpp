5b7b7c558e628c1aeef43ee506e7fc0f69876cef
ben <ben.lee8602@gmail.com>
2025-03-30 02:38:50 -0400

    engine window

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,5 +1,151 @@
-#include <iostream>
+#include <chrono>
+
+#include <ramiel/graphics.h>
+#include <ramiel/physics.h>
+
+#include "window.h"
+using namespace ramiel;
+
+
+void cameraControls(float dtime) {
+    constexpr float cameraSpeedNormal = 2.0f;
+    constexpr float cameraSpeedFaster = 10.0f;
+    constexpr float cameraRotationSpeed = 1.57079f;
+
+    // reset pos and rot
+    if (keyPressed(Key::Q)) {
+        setPos(Vec3f());
+        setRot(Vec3f());
+        return;
+    }
+
+    // move faster
+    float cameraSpeed = (
+        keyPressed(Key::SHIFT) ?
+        cameraSpeedFaster : cameraSpeedNormal
+    );
+    float dPos = cameraSpeed * dtime;
+    float dRot = cameraRotationSpeed * dtime;
+
+    Vec3f pos = getPos();
+    Vec3f rot = getRot();
+
+    // move left
+    if (keyPressed(Key::A)) {
+        pos[X] -= dPos * std::cos(rot[Y]);
+        pos[Z] -= dPos * std::sin(rot[Y]);
+    }
+
+    // move right
+    if (keyPressed(Key::D)) {
+        pos[X] += dPos * std::cos(rot[Y]);
+        pos[Z] += dPos * std::sin(rot[Y]);
+    }
+
+    // move down
+    if (keyPressed(Key::CONTROL)) {
+        pos[Y] -= dPos;
+    }
+
+    // move up
+    if (keyPressed(Key::SPACE)) {
+        pos[Y] += dPos;
+    }
+
+    // move backward
+    if (keyPressed(Key::S)) {
+        pos[X] += dPos * std::sin( rot[Y]);
+        pos[Y] += dPos * std::sin(-rot[X]);
+        pos[Z] -= dPos * std::cos( rot[Y]);
+    }
+
+    // move forward
+    if (keyPressed(Key::W)) {
+        pos[X] -= dPos * std::sin( rot[Y]);
+        pos[Y] -= dPos * std::sin(-rot[X]);
+        pos[Z] += dPos * std::cos( rot[Y]);
+    }
+
+    // turn down
+    if (keyPressed(Key::DOWN)) {
+        rot[X] = std::max(rot[X] - dRot, -1.57079f);
+    }
+
+    // turn up
+    if (keyPressed(Key::UP)) {
+        rot[X] = std::min(rot[X] + dRot, 1.57079f);
+    }
+
+    // turn right
+    if (keyPressed(Key::RIGHT)) {
+        rot[Y] -= dRot;
+    }
+
+    // turn left
+    if (keyPressed(Key::LEFT)) {
+        rot[Y] += dRot;
+    }
+
+    setPos(pos);
+    setRot(rot);
+}
+
+
+void getFrameRGB(uint8_t* frame) {
+    auto color = getColorBuffer();
+    for (size_t i = 0; i < getBufferSize(); ++i) {
+        *frame++ = std::min(color[i][B], 255.0f);
+        *frame++ = std::min(color[i][G], 255.0f);
+        *frame++ = std::min(color[i][R], 255.0f);
+    }
+}
+
 
 int main() {
-    std::cout << "engine!!!\n";
+    if (!initWindow()) return 0;
+
+    setRes({
+        static_cast<unsigned>(windowWidth()),
+        static_cast<unsigned>(windowHeight())
+    });
+
+    const char* meshFile = "example/assets/models/cube.obj";
+    const char* textureFile = "example/assets/textures/brickwall_texture.jpg";
+
+    auto mesh = std::make_shared<Mesh>(meshFile);
+    auto texture = std::make_shared<Texture>(textureFile, rgb1);
+
+    Entity entity(
+        mesh,
+        std::make_unique<VertexShaderTextured>(translate(Vec3f{ 0, 0, 4 })),
+        std::make_unique<PixelShaderTextured>(texture, 8.0f, 1.0f, Vec3f{})
+    );
+
+    setAmbientLight({ 25, 10, 20 });
+    addLight(new PointLight(Vec3f{ 255, 100, 200 }, 4.0f, Vec3f{ 1, 1.5, 2 }, 0.5f));
+
+    std::vector<uint8_t> frame(windowWidth() * windowHeight() * 3);
+    auto frameTimeStart = std::chrono::steady_clock::now();
+    auto frameTimeEnd = std::chrono::steady_clock::now();
+
+    while (windowGood()) {
+        pollWindowMessages();
+
+        frameTimeEnd = std::chrono::steady_clock::now();
+        double dtime = (frameTimeEnd - frameTimeStart).count() / 1e9;
+        frameTimeStart = frameTimeEnd;
+
+        cameraControls((float)dtime);
+
+        simulatePhysics(dtime);
+
+        std::fill(getColorBuffer(), getColorBuffer() + getBufferSize(), Vec3f{});
+        std::fill(getDepthBuffer(), getDepthBuffer() + getBufferSize(), getZ1());
+        entity.draw();
+        getFrameRGB(frame.data());
+
+        drawToWindow(frame.data());
+    }
+
+    return 0;
 }
---
+++ src/engine/window.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+
+namespace ramiel {
+
+    enum class Key : uint8_t {
+        MOUSELEFT, MOUSEMIDDLE, MOUSERIGHT, // mouse
+        ESCAPE, BACKSPACE, TAB, ENTER, SPACE, // special
+        SHIFT, CONTROL, ALT, // modifier
+        LEFT, RIGHT, DOWN, UP, // navigation
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, // numeric
+        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, // alphabetic
+        F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, // function
+        TICK, MINUS, PLUS, BRACKETOPEN, BRACKETCLOSE, BACKSLASH, COLON, QUOTE, COMMA, PERIOD, SLASH // misc
+    };
+
+    bool initWindow();
+    bool windowGood();
+    void pollWindowMessages();
+
+    size_t windowWidth();
+    size_t windowHeight();
+
+    bool keyDown(Key key);
+    bool keyPressed(Key key);
+    bool keyUp(Key key);
+
+    void drawToWindow(uint8_t* frame);
+
+}
